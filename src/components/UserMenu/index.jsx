6868dd46b32876ccaa6c8c6b00b46a4391784b04
syntaxval <syntaxval@users.noreply.github.com>
2018-10-03 13:18:47 +0200

    Revert to missing handleClose for menu items

--- src/components/UserMenu/index.jsx
+++ src/components/UserMenu/index.jsx
@@ -97,10 +97,10 @@ class UserMenu extends Component {
                     </MenuItem>
                     <Divider />
                     <MenuItem className={classes.menuItem} component={Link}
-                        to="/dashboard/user"
+                        to="/dashboard/user" onClick={this.handleClose}
                     >Profile</MenuItem>
                     <MenuItem className={classes.menuItem} component={Link}
-                        to="/dashboard/account"
+                        to="/dashboard/account" onClick={this.handleClose}
                     >Account</MenuItem>
                     <MenuItem className={classes.menuItem} onClick={(event) =>
                         this.handleClose(event, this.logout)}
