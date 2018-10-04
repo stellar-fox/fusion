c648c5c45350f466a6b1217f9b3b54ee5650ab96
syntaxval <syntaxval@users.noreply.github.com>
2018-10-04 21:57:34 +0200

    Add PhotoAvatar component

---
+++ src/components/PhotoAvatar/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react"
+import { bindActionCreators, compose } from "redux"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import Avatar from "@material-ui/core/Avatar"
+import classNames from "classnames"
+
+
+// <PhotoAvatar> component
+export default compose(
+    withStyles({
+        avatar: {
+            border: "1px solid rgba(255,255,255,0.1)",
+        },
+        square: {
+            borderRadius: 3,
+        },
+    }),
+    connect(
+        (_state) => ({}),
+        (dispatch) => bindActionCreators({}, dispatch)
+    )
+)(
+    class extends Component {
+
+        // ...
+        static propTypes = {
+            classes: PropTypes.object.isRequired,
+            src: PropTypes.string.isRequired,
+        }
+
+
+        // ...
+        render = () => (
+            ({ classes, src, size, variant, }) =>
+                <Avatar
+                    style={{
+                        margin: "10", width: size || 40,
+                        height: size || 40,
+                    }}
+                    alt="Avatar"
+                    className={classNames(classes.avatar, classes[variant])}
+                    src={src}
+                />
+        )(this.props)
+
+    }
+)
