1ddf920e8d74a8f4472ccd7cf842282d9f1277af
syntaxval <syntaxval@users.noreply.github.com>
2018-12-18 18:19:33 +0100

    Add opacity motion to account card.

--- src/components/Accounts/AccountCard.jsx
+++ src/components/Accounts/AccountCard.jsx
@@ -3,7 +3,7 @@ import { bindActionCreators, compose } from "redux"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import { withStyles } from "@material-ui/core/styles"
-
+import { Motion, presets, spring } from "react-motion"
 import {
     Avatar,
     Card,
@@ -81,69 +81,76 @@ export default compose(
         // ...
         render = () => (
             ({ accountId, accountType, classes }) =>
-                <Card className={classnames(func.choose(accountType, {
-                    [at.REAL]: () => classes.cardReal,
-                    [at.DEMO]: () => classes.cardDemo,
-                }, () => "unknown account type"), classes.card)}
+                <Motion defaultStyle={{ opacity: 0 }}
+                    style={{ opacity: spring(1, presets.stiff) }}
                 >
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="Account Type"
-                                className={func.choose(accountType, {
-                                    [at.REAL]: () => classes.avatarReal,
-                                    [at.DEMO]: () => classes.avatarDemo,
-                                }, () => "unknown account type")}
+                    {value =>
+                        <Card style={{ opacity: value.opacity }}
+                            className={classnames(func.choose(accountType, {
+                                [at.REAL]: () => classes.cardReal,
+                                [at.DEMO]: () => classes.cardDemo,
+                            }, () => "unknown account type"), classes.card)}
+                        >
+                            <CardHeader
+                                avatar={
+                                    <Avatar aria-label="Account Type"
+                                        className={func.choose(accountType, {
+                                            [at.REAL]: () => classes.avatarReal,
+                                            [at.DEMO]: () => classes.avatarDemo,
+                                        }, () => "unknown account type")}
+                                    />
+                                }
+                                action={
+                                    <IconButton style={{ marginLeft: "1rem" }} aria-label="Edit"
+                                        classes={{ root: func.choose(accountType, {
+                                            [at.REAL]: () => classes.iconButtonReal,
+                                            [at.DEMO]: () => classes.iconButtonDemo,
+                                        }, () => "unknown account type") }}
+                                    >
+                                        <EditRounded />
+                                    </IconButton>
+                                }
+                                title="No Name"
+                                subheader={accountId && shorten(accountId, 11, shorten.MIDDLE, "-")}
                             />
-                        }
-                        action={
-                            <IconButton style={{ marginLeft: "1rem" }} aria-label="Edit"
-                                classes={{ root: func.choose(accountType, {
-                                    [at.REAL]: () => classes.iconButtonReal,
-                                    [at.DEMO]: () => classes.iconButtonDemo,
-                                }, () => "unknown account type") }}
-                            >
-                                <EditRounded />
-                            </IconButton>
-                        }
-                        title="No Name"
-                        subheader={accountId && shorten(accountId, 11, shorten.MIDDLE, "-")}
-                    />
-                    <CardContent classes={{ root: classes.cardContent }}>
-                        <div>
-                            <Typography variant="h4">
-                                Current Balance
-                            </Typography>
-                            <Typography variant="h5">
-                                $1,234,567.89
-                            </Typography>
-                        </div>
-                        <div style={{ marginLeft: "1rem", width: 80 }}>
-                            <Sparklines data={[5, 10, 15, 9, 20, 28, 30, 28, 20, 25]}>
-                                <SparklinesLine style={{ stroke: "#8ed53f", strokeWidth: "2", fill: "#8fc638", fillOpacity: "0.2" }} />
-                                <SparklinesReferenceLine type="mean" style={{ stroke: "white", strokeOpacity: "0.9", strokeDasharray: "2, 2" }} />
-                            </Sparklines>
-                        </div>
-                    </CardContent>
+                            <CardContent classes={{ root: classes.cardContent }}>
+                                <div>
+                                    <Typography variant="h4">
+                                        Current Balance
+                                    </Typography>
+                                    <Typography variant="h5">
+                                        $1,234,567.89
+                                    </Typography>
+                                </div>
+                                <div style={{ marginLeft: "1rem", width: 80 }}>
+                                    <Sparklines data={[5, 10, 15, 9, 20, 28, 30, 28, 20, 25]}>
+                                        <SparklinesLine style={{ stroke: "#8ed53f", strokeWidth: "2", fill: "#8fc638", fillOpacity: "0.2" }} />
+                                        <SparklinesReferenceLine type="mean" style={{ stroke: "white", strokeOpacity: "0.9", strokeDasharray: "2, 2" }} />
+                                    </Sparklines>
+                                </div>
+                            </CardContent>
 
-                    <CardActions className={classes.actions} disableActionSpacing>
-                        <IconButton aria-label="Pay"
-                            classes={{ root: func.choose(accountType, {
-                                [at.REAL]: () => classes.iconButtonReal,
-                                [at.DEMO]: () => classes.iconButtonDemo,
-                            }, () => "unknown account type") }}
-                        >
-                            <PaymentRounded />
-                        </IconButton>
-                        <IconButton aria-label="Fund"
-                            classes={{ root: func.choose(accountType, {
-                                [at.REAL]: () => classes.iconButtonReal,
-                                [at.DEMO]: () => classes.iconButtonDemo,
-                            }, () => "unknown account type") }}
-                        >
-                            <AccountBalanceWalletRounded />
-                        </IconButton>
-                    </CardActions>
-                </Card>
+                            <CardActions className={classes.actions} disableActionSpacing>
+                                <IconButton aria-label="Pay"
+                                    classes={{ root: func.choose(accountType, {
+                                        [at.REAL]: () => classes.iconButtonReal,
+                                        [at.DEMO]: () => classes.iconButtonDemo,
+                                    }, () => "unknown account type") }}
+                                >
+                                    <PaymentRounded />
+                                </IconButton>
+                                <IconButton aria-label="Fund"
+                                    classes={{ root: func.choose(accountType, {
+                                        [at.REAL]: () => classes.iconButtonReal,
+                                        [at.DEMO]: () => classes.iconButtonDemo,
+                                    }, () => "unknown account type") }}
+                                >
+                                    <AccountBalanceWalletRounded />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    }
+                </Motion>
         )(this.props)
 
     }
