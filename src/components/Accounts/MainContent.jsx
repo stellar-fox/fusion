2b3afd20ef6428de691c2de4e9c5c781ed77de10
syntaxval <syntaxval@users.noreply.github.com>
2019-01-04 11:39:54 +0700

    Refactor index into MainComponent using hooks.

---
+++ src/components/Accounts/MainContent.jsx
@@ -0,0 +1,248 @@
+import React, { Fragment } from "react"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { withStyles } from "@material-ui/core/styles"
+import withWidth, { isWidthUp } from "@material-ui/core/withWidth"
+import {
+    Fab,
+    Grid,
+    Tab,
+    Tabs,
+    Typography,
+    Zoom,
+} from "@material-ui/core"
+import { AddRounded } from "@material-ui/icons"
+import SwipeableViews from "react-swipeable-views"
+import AccountCard from "./AccountCard"
+import { Motion, presets, spring } from "react-motion"
+import {
+    func,
+} from "@xcmats/js-toolbox"
+import {
+    getDemoAccounts,
+    getRealAccounts,
+} from "../../lib/logic/stellarAccount"
+import { accountType as at } from "../../redux/Accounts"
+import { action as AccountsActions } from "../../redux/Accounts"
+
+
+
+
+/**
+ * Fusion.
+ *
+ * Main content view.
+ *
+ * @module Accounts
+ * @license Apache-2.0
+ */
+const MainContent = ({
+    changeTab, classes, demoAccounts, realAccounts, tabSelected, width,
+}) => {
+    
+    const transitionDuration = {
+        enter: 225,
+        exit: 195,
+    }
+
+    return <Motion defaultStyle={{ x: -10, opacity: 0 }}
+        style={{
+            x: spring(0, presets.stiff),
+            opacity: spring(1),
+        }}
+    >
+        {value =>
+            <Fragment>
+                <Grid
+                    container
+                    direction={"column"}
+                    wrap={"nowrap"}
+                    style={{ position: "relative" }}
+                    className="m-b"
+                >
+                    <Grid item>
+                        <Typography style={{
+                            position: "relative",
+                            WebkitTransform: `translate(${value.x}px, 0)`,
+                            transform: `translate(${value.x}px, 0)`,
+                            opacity: value.opacity,
+                        }} variant="h6"
+                        >
+                            Accounts
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography style={{
+                            position: "relative",
+                            WebkitTransform: `translate(${value.x}px, 0)`,
+                            transform: `translate(${value.x}px, 0)`,
+                            opacity: value.opacity,
+                        }} variant="h4"
+                        >
+                            Manage all your accounts in one place.
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Zoom
+                            key="add-real"
+                            in={tabSelected === 0}
+                            timeout={transitionDuration}
+                            style={{
+                                transitionDelay: `${tabSelected === 0 ?
+                                    transitionDuration.exit : 0}ms`,
+                            }}
+                            unmountOnExit
+                        >
+                            <Fab aria-label="Add Real" size="small"
+                                className={classes.fab}
+                                classes={{ root: classes.fabReal }}
+                            >
+                                <AddRounded />
+                            </Fab>
+                        </Zoom>
+
+                        <Zoom
+                            key="add-demo"
+                            in={tabSelected === 1}
+                            timeout={transitionDuration}
+                            style={{
+                                transitionDelay: `${tabSelected === 1 ?
+                                    transitionDuration.exit : 0}ms`,
+                            }}
+                            unmountOnExit
+                        >
+                            <Fab aria-label="Add Demo" size="small"
+                                className={classes.fab}
+                                classes={{ root: classes.fabDemo }}
+                            >
+                                <AddRounded />
+                            </Fab>
+                        </Zoom>
+                    </Grid>
+                </Grid>
+
+                <Tabs
+                    style={{
+                        position: "relative",
+                        WebkitTransform: `translate(${value.x}px, 0)`,
+                        transform: `translate(${value.x}px, 0)`,
+                        opacity: value.opacity,
+                    }}
+                    value={tabSelected}
+                    onChange={(_e, value) => changeTab(value)}
+                    variant="fullWidth"
+                    classes={{ indicator: classes.indicator }}
+                >
+                    <Tab classes={{ label: classes.labelRealAccounts }}
+                        label="Real"
+                    />
+                    <Tab classes={{ label: classes.labelDemoAccounts }}
+                        label="Demo"
+                    />
+                </Tabs>
+
+                <SwipeableViews
+                    style={{
+                        position: "relative",
+                        WebkitTransform: `translate(${value.x}px, 0)`,
+                        transform: `translate(${value.x}px, 0)`,
+                        opacity: value.opacity,
+                    }}
+                    index={tabSelected}
+                >
+                    <Grid
+                        container
+                        direction={isWidthUp("md", width) ? "row" : "column"}
+                        spacing={8}
+                        style={{ position: "relative" }}
+                        className="m-t m-b"
+                        classes={{ "spacing-xs-8": classes.spacing }}
+                    >
+                        {realAccounts.map((account) =>
+                            <Grid key={`${account.accountId}-${at.REAL}`} item>
+                                <AccountCard accountId={account.accountId}
+                                    accountType={at.REAL}
+                                />
+                            </Grid>
+                        )}
+                    </Grid>
+
+                    <Grid
+                        container
+                        direction={isWidthUp("md", width) ? "row" : "column"}
+                        spacing={8}
+                        style={{ position: "relative" }}
+                        className="m-t m-b"
+                        classes={{ "spacing-xs-8": classes.spacing }}
+                    >
+                        {demoAccounts.map((account) =>
+                            <Grid key={`${account.accountId}-${at.DEMO}`} item>
+                                <AccountCard accountId={account.accountId}
+                                    accountType={at.DEMO}
+                                />
+                            </Grid>
+                        )}
+                    </Grid>
+                </SwipeableViews>
+            </Fragment>
+        }
+    </Motion>    
+}
+
+
+// ...
+export default func.compose(
+    withStyles((theme) => ({
+
+        fab: {
+            position: "absolute",
+            right: "1rem",
+            top: "0.5rem",
+        },
+
+        fabDemo: {
+            backgroundColor: theme.palette.error.dark,
+            color: theme.palette.common.white,
+            "&:hover": {
+                backgroundColor: theme.palette.error.main,
+            },
+        },
+
+        fabReal: {
+            backgroundColor: theme.palette.custom.greenDark,
+            color: theme.palette.common.white,
+            "&:hover": {
+                backgroundColor: theme.palette.custom.green,
+            },
+        },
+
+        indicator: {
+            backgroundColor: theme.palette.custom.greenDark,
+        },
+
+        labelRealAccounts: {
+            color: theme.palette.custom.green,
+        },
+
+        labelDemoAccounts: {
+            color: theme.palette.error.main,
+        },
+
+        spacing: {
+            width: "100%",
+            margin: "0.5rem 0 0 0",
+        },
+
+    })),
+    withWidth(),
+    connect(
+        (state) => ({
+            tabSelected: state.Accounts.tabSelected,
+            realAccounts: getRealAccounts(state.StellarAccounts),
+            demoAccounts: getDemoAccounts(state.StellarAccounts),
+        }),
+        (dispatch) => bindActionCreators({
+            changeTab: AccountsActions.changeTab,
+        }, dispatch)
+    ),
+)(MainContent)
--- src/components/Accounts/index.jsx
+++ src/components/Accounts/index.jsx
@@ -3,40 +3,28 @@ import { bindActionCreators, compose } from "redux"
 import { connect } from "react-redux"
 import { withStyles } from "@material-ui/core/styles"
 import { Redirect, Route } from "react-router-dom"
-import withWidth, { isWidthUp } from "@material-ui/core/withWidth"
-import {
-    Fab,
-    Grid,
-    Paper,
-    Tab,
-    Tabs,
-    Typography,
-    Zoom,
-} from "@material-ui/core"
-import { AddRounded } from "@material-ui/icons"
+import withWidth from "@material-ui/core/withWidth"
+import { Paper } from "@material-ui/core"
 import { ConnectedSwitch as Switch, resolvePath } from "../FusionRouter"
-import { Motion, presets, spring } from "react-motion"
 import { action as AccountsActions } from "../../redux/Accounts"
-import SwipeableViews from "react-swipeable-views"
 import {
     getDemoAccounts,
     getRealAccounts,
 } from "../../lib/logic/stellarAccount"
-import AccountCard from "./AccountCard"
-import { accountType as at } from "../../redux/Accounts"
 import Awaiter from "../Awaiter"
 
 import { listenForStellarAccountsChange } from "../../actions/stellarAccount"
 import { listenForSigningMethodsChange } from "../../actions/signingMethods"
 import ModalEditName from "./ModalEditName"
 import Snacky from "../../lib/mui-v1/Snacky"
+import MainContent from "./MainContent"
 
 
 
 
 // <Accounts> component
 export default compose(
-    withStyles((theme) => ({
+    withStyles((_theme) => ({
 
         awaiter: {
             position: "absolute",
@@ -47,47 +35,8 @@ export default compose(
             margin: "auto",
         },
 
-        fab: {
-            position: "absolute",
-            right: "1rem",
-            top: "0.5rem",
-        },
-
-        fabDemo: {
-            backgroundColor: theme.palette.error.dark,
-            color: theme.palette.common.white,
-            "&:hover": {
-                backgroundColor: theme.palette.error.main,
-            },
-        },
-
-        fabReal: {
-            backgroundColor: theme.palette.custom.greenDark,
-            color: theme.palette.common.white,
-            "&:hover": {
-                backgroundColor: theme.palette.custom.green,
-            },
-        },
-
-        indicator: {
-            backgroundColor: theme.palette.custom.greenDark,
-        },
-
-        labelRealAccounts: {
-            color: theme.palette.custom.green,
-        },
-
-        labelDemoAccounts: {
-            color: theme.palette.error.main,
-        },
-
         paperCanvas: {
             padding: "10px",
-        },
-
-        spacing: {
-            width: "100%",
-            margin: "0.5rem 0 0 0",
         },
 
     })),
@@ -128,17 +77,8 @@ export default compose(
 
 
         // ...
-        onTabChange = (_event, value) => this.props.changeTab(value)
-
-
-        // ...
         render = () => (
-            ({ classes, demoAccounts, loading, realAccounts, tabSelected, width }) => {
-
-                const transitionDuration = {
-                    enter: 225,
-                    exit: 195,
-                }
+            ({ classes, loading }) => {
 
                 return <Switch>
                     <Route exact path={this.rr(".")}>
@@ -148,139 +88,7 @@ export default compose(
                             <Snacky />
                             <ModalEditName />
                             <Paper className={classes.paperCanvas}>
-
-                                <Motion defaultStyle={{ x: -10, opacity: 0 }}
-                                    style={{
-                                        x: spring(0, presets.stiff),
-                                        opacity: spring(1),
-                                    }}
-                                >
-                                    {value =>
-                                        <Fragment>
-                                            <Grid
-                                                container
-                                                direction={"column"}
-                                                wrap={"nowrap"}
-                                                style={{ position: "relative" }}
-                                                className="m-b"
-                                            >
-                                                <Grid item>
-                                                    <Typography style={{
-                                                        position: "relative",
-                                                        WebkitTransform: `translate(${value.x}px, 0)`,
-                                                        transform: `translate(${value.x}px, 0)`,
-                                                        opacity: value.opacity,
-                                                    }} variant="h6"
-                                                    >
-                                                        Accounts
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Typography style={{
-                                                        position: "relative",
-                                                        WebkitTransform: `translate(${value.x}px, 0)`,
-                                                        transform: `translate(${value.x}px, 0)`,
-                                                        opacity: value.opacity,
-                                                    }} variant="h4"
-                                                    >
-                                                        Manage all your accounts in one place.
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Zoom
-                                                        key="add-real"
-                                                        in={tabSelected === 0}
-                                                        timeout={transitionDuration}
-                                                        style={{
-                                                            transitionDelay: `${tabSelected === 0 ? transitionDuration.exit : 0}ms`,
-                                                        }}
-                                                        unmountOnExit
-                                                    >
-                                                        <Fab aria-label="Add Real" size="small" className={classes.fab} classes={{ root: classes.fabReal }}>
-                                                            <AddRounded />
-                                                        </Fab>
-                                                    </Zoom>
-
-                                                    <Zoom
-                                                        key="add-demo"
-                                                        in={tabSelected === 1}
-                                                        timeout={transitionDuration}
-                                                        style={{
-                                                            transitionDelay: `${tabSelected === 1 ? transitionDuration.exit : 0}ms`,
-                                                        }}
-                                                        unmountOnExit
-                                                    >
-                                                        <Fab aria-label="Add Demo" size="small" className={classes.fab} classes={{ root: classes.fabDemo }}>
-                                                            <AddRounded />
-                                                        </Fab>
-                                                    </Zoom>
-                                                </Grid>
-                                            </Grid>
-
-
-
-                                            <Tabs
-                                                style={{
-                                                    position: "relative",
-                                                    WebkitTransform: `translate(${value.x}px, 0)`,
-                                                    transform: `translate(${value.x}px, 0)`,
-                                                    opacity: value.opacity,
-                                                }}
-                                                value={tabSelected}
-                                                onChange={this.onTabChange}
-                                                variant="fullWidth"
-                                                classes={{ indicator: classes.indicator }}
-                                            >
-                                                <Tab classes={{ label: classes.labelRealAccounts }} label="Real" />
-                                                <Tab classes={{ label: classes.labelDemoAccounts }} label="Demo" />
-                                            </Tabs>
-
-                                            
-                                            <SwipeableViews
-                                                style={{
-                                                    position: "relative",
-                                                    WebkitTransform: `translate(${value.x}px, 0)`,
-                                                    transform: `translate(${value.x}px, 0)`,
-                                                    opacity: value.opacity,
-                                                }}
-                                                index={tabSelected}
-                                            >
-                                                <Grid
-                                                    container
-                                                    direction={isWidthUp("md", width) ? "row" : "column"}
-                                                    spacing={8}
-                                                    style={{ position: "relative"}}
-                                                    className="m-t m-b"
-                                                    classes={{ "spacing-xs-8": classes.spacing }}
-                                                >
-                                                    {realAccounts.map((account) =>
-                                                        <Grid key={`${account.accountId}-${at.REAL}`} item>
-                                                            <AccountCard accountId={account.accountId} accountType={at.REAL} />
-                                                        </Grid>
-                                                    )}
-                                                </Grid>
-
-                                                <Grid
-                                                    container
-                                                    direction={isWidthUp("md", width) ? "row" : "column"}
-                                                    spacing={8}
-                                                    style={{ position: "relative" }}
-                                                    className="m-t m-b"
-                                                    classes={{ "spacing-xs-8": classes.spacing }}
-                                                >
-                                                    {demoAccounts.map((account) =>
-                                                        <Grid key={`${account.accountId}-${at.DEMO}`} item>
-                                                            <AccountCard accountId={account.accountId} accountType={at.DEMO} />
-                                                        </Grid>
-                                                    )}
-                                                </Grid>
-
-                                            </SwipeableViews>
-                                        </Fragment>
-
-                                    }
-                                </Motion>
-                        
+                                <MainContent />
                             </Paper>
                         </Fragment>
                         }
