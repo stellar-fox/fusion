81e322f86a05de7ade4c922073aec349465a0980
syntaxval <syntaxval@users.noreply.github.com>
2019-01-18 15:15:36 +0700

    Add user login/authentication thunks.

--- src/thunks/main.js
+++ src/thunks/main.js
@@ -1,5 +1,105 @@
-import { action as AwaiterActions } from "../redux/Awaiter"
 import { action as AppActions } from "../redux/Fusion"
+import { action as AuthActions } from "../redux/Auth"
+import { action as AwaiterActions } from "../redux/Awaiter"
+import { action as UserLoginActions } from "../redux/UserLogin"
+import { authenticate, storageRef } from "../firebase"
+import { string } from "@xcmats/js-toolbox"
+
+
+
+
+export const doAuthenticate = () =>
+    async (dispatch, getState) => {
+        try {
+            console.log("Attempting to authenticate ...")
+            const { email, password } = getState().UserLogin
+
+            await dispatch(UserLoginActions.setState({
+                disabled: true,
+                errorEmail: false,
+                errorMessageEmail: string.empty(),
+                errorPassword: false,
+                errorMessagePassword: string.empty(),
+                progressBarOpacity: 1,
+            }))
+            
+            const auth = await authenticate(email, password)
+            const jwt = await auth.user.getIdToken()
+
+            await dispatch(AuthActions.setState({
+                uid: auth.user.uid,
+                email: auth.user.email,
+                displayName: auth.user.displayName || string.empty(),
+                photoUrl: auth.user.photoURL || string.empty(),
+                emailVerified: auth.user.emailVerified,
+                jwt,
+            }))
+
+            console.log("Got user credentials.")
+            
+            !auth.user.photoURL &&
+                await dispatch(getStorageAvatar(auth.user))
+            
+            console.log("Logged in.")
+            await dispatch(UserLoginActions.resetState())
+
+        } catch (error) {
+
+            await dispatch(UserLoginActions.setState({
+                progressBarOpacity: 0,
+                disabled: false,
+            }))
+
+            // handle error on UI based on error code
+            if (error.code === "auth/invalid-email") {
+                await dispatch(UserLoginActions.setState({
+                    errorEmail: true,
+                    errorMessageEmail: error.message,
+                    errorPassword: false,
+                    errorMessagePassword: string.empty(),
+                }))
+                return
+            }
+
+            if (error.code === "auth/wrong-password") {
+                await dispatch(UserLoginActions.setState({
+                    errorEmail: false,
+                    errorMessagePassword: "Password is invalid.",
+                    errorPassword: true,
+                    errorMessageEmail: string.empty(),
+                }))
+                return
+            }
+
+            if (error.code === "auth/user-not-found") {
+                await dispatch(UserLoginActions.setState({
+                    errorEmail: true,
+                    errorPassword: true,
+                    errorMessageEmail: "Invalid credentials.",
+                    errorMessagePassword: "Invalid credentials.",
+                }))
+                return
+            }
+
+            // in case of other error - display the code/message
+            await dispatch(UserLoginActions.setState({
+                errorEmail: true,
+                errorPassword: true,
+                errorMessageEmail: error.code,
+                errorMessagePassword: error.message,
+            }))
+        }
+    }
+
+
+
+
+export const getStorageAvatar = (user) =>
+    async (dispatch, _getState) =>
+        storageRef().child(`${user.uid}/avatar.jpeg`)
+            .getDownloadURL().then((photoUrl) => {
+                dispatch(AuthActions.setState({ photoUrl }))
+            })
 
 
 
@@ -27,4 +127,12 @@ export const setAwaiterLoading = (message) =>
         dispatch(AwaiterActions.setLoading())
         dispatch(AwaiterActions.showSpinner())
         dispatch(AwaiterActions.setProgressMessage(message))
+    }
+
+
+
+
+export const clearAwaiter = () =>
+    async (dispatch, _getState) => {
+        dispatch(AwaiterActions.resetState())
     }
