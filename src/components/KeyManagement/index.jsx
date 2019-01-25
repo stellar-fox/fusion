3aea1d86d3aae083b76301919af9b8fa6365675f
syntaxval <syntaxval@users.noreply.github.com>
2019-01-25 13:34:49 +0700

    Remove ReactMotion. Align view title display.

--- src/components/KeyManagement/index.jsx
+++ src/components/KeyManagement/index.jsx
@@ -5,7 +5,6 @@ import { withStyles } from "@material-ui/core/styles"
 import { Redirect, Route } from "react-router-dom"
 import { Paper, Typography } from "@material-ui/core"
 import { ConnectedSwitch as Switch, resolvePath } from "../FusionRouter"
-import { Motion, presets, spring } from "react-motion"
 import ShambhalaPureCard from "./ShambhalaPureCard"
 import ShambhalaLedgerCard from "./ShambhalaLedgerCard"
 import ShambhalaScepticCard from "./ShambhalaScepticCard"
@@ -50,47 +49,34 @@ export default compose(
 
                         <Paper className={classes.paperCanvas}>
                             <Snacky />
-                            <Motion defaultStyle={{ x: -10, opacity: 0 }}
-                                style={{
-                                    x: spring(0, presets.stiff),
-                                    opacity: spring(1),
-                                }}
-                            >
-                                {value =>
-                                    <Fragment>
-                                        <Typography style={{
-                                            position: "relative",
-                                            WebkitTransform: `translate(${value.x}px, 0)`,
-                                            transform: `translate(${value.x}px, 0)`,
-                                            opacity: value.opacity,
-                                        }} variant="h6"
-                                        >
-                                            Key Management
-                                        </Typography>
-                                        <div style={{
-                                            position: "relative",
-                                            WebkitTransform: `translate(${value.x}px, 0)`,
-                                            transform: `translate(${value.x}px, 0)`,
-                                            opacity: value.opacity,
-                                        }} className={isWidthDown("md", width) ?
-                                            "m-t m-b flex-box-col items-centered" :
-                                            "m-t m-b flex-box-row space-around"}
-                                        >
-                                            <ShambhalaPureCard />
+                            
+                            <Fragment>
+                                <Typography variant="h6">
+                                    Key Management
+                                </Typography>
+                                <Typography variant="h4">
+                                    Manage additional keys with your account.
+                                </Typography>
+                                <div style={{
+                                    position: "relative",
+                                }} className={isWidthDown("md", width) ?
+                                    "m-t m-b flex-box-col items-centered" :
+                                    "m-t m-b flex-box-row space-around"}
+                                >
+                                    <ShambhalaPureCard />
 
-                                            {isWidthDown("md", width) &&
-                                                <div className="m-b"></div>}
+                                    {isWidthDown("md", width) &&
+                                        <div className="m-b"></div>}
 
-                                            <ShambhalaLedgerCard />
+                                    <ShambhalaLedgerCard />
 
-                                            {isWidthDown("md", width) &&
-                                                <div className="m-b"></div>}
+                                    {isWidthDown("md", width) &&
+                                        <div className="m-b"></div>}
 
-                                            <ShambhalaScepticCard />
-                                        </div>
-                                    </Fragment>
-                                }
-                            </Motion>
+                                    <ShambhalaScepticCard />
+                                </div>
+                            </Fragment>
+                                
                         </Paper>
 
                     </Route>
