96cfe8d73db746324d16808b3c3afde4b47685a5
syntaxval <syntaxval@users.noreply.github.com>
2019-02-05 15:44:11 +0700

    Add saving account data thunk.

--- src/thunks/AddAccount/index.js
+++ src/thunks/AddAccount/index.js
@@ -11,11 +11,16 @@
 import { actions as AddAccountActions } from "../../redux/AddAccount"
 import { string } from "@xcmats/js-toolbox"
 import { Shambhala } from "../../lib/shambhala.client"
+import { update } from "../../firebase"
 import { config } from "../../firebase/config"
 import {
     clearAwaiter,
     setAwaiterLoading,
 } from "../main"
+import { Networks } from "stellar-sdk"
+import { AccountTypes } from "../../redux/Accounts"
+import { signingMethod as sm } from "../../redux/Keys"
+
 
 
 
@@ -23,6 +28,7 @@ import {
 const context = {
     shambhala: null,
 }
+
 
 
 
@@ -117,8 +123,44 @@ export const next = () =>
  * 
  */
 export const saveAccountData = () =>
-    async (_dispatch, _getState) => {
+    async (_dispatch, getState) => {
+        const
+            {
+                accountId,
+                name,
+                networkPassphrase,
+                signingMethod,
+            } = getState().AddAccount,
 
+            { uid } = getState().Auth,
+
+            {
+                account,
+                useDefaultAccount,
+            } = getState().LedgerHQ
+
+
+        await update(`user/${uid}/stellarAccounts/${accountId}`, {
+            accountId,
+            name,
+            networkPassphrase,
+            createdAt: Date.now(),
+        })
+
+
+        await update(
+            `user/${uid}/signingMethods/${accountId}/${signingMethod}`, {
+                createdAt: Date.now(),
+            }
+        )
+
+
+        signingMethod === sm.LEDGERHQ &&
+            await update(
+                `user/${uid}/signingMethods/${accountId}/${signingMethod}`, {
+                    account: useDefaultAccount ? "0" : account,
+                }
+            )
     }
 
 
@@ -129,10 +171,20 @@ export const saveAccountData = () =>
  */
 export const generateAccountId = () =>
     async (dispatch, getState) => {
+
+        // 0. Set Network passphrase based on accountType
+        const { accountType } = getState().AddAccount
+        await dispatch(await AddAccountActions.setNetworkPassphrase(
+            accountType === AccountTypes.REAL ? Networks.PUBLIC : Networks.TESTNET
+        ))
+                
+        // 1. Instantiate Shambhala
         context.shambhala = await new Shambhala(
             config.shambhala.client,
             { token: jwt }
         )
+
+        // 2. Obtain accountId from Shambhala
         const
             { jwt } = getState().Auth,
             accountId = await context.shambhala.generateAddress()
@@ -162,6 +214,7 @@ export const runAddAccountRecipe = () =>
         // 2. generate new accountId using Shambhala
         await dispatch(await generateAccountId())
         // 3. save account data
+        await dispatch(await setAwaiterLoading("Saving account data ..."))
         await dispatch(await saveAccountData())
         // 4. clear Redux child data by calling cancel action
         await dispatch(await cancel())
